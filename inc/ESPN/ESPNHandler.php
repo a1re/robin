8a71ed8ea3b5bbdc570a04abe59f42284d3a1952
Yuriy Marin (sprinthost) <yuriy.marin@gmail.com>
2019-08-22 18:35:33 +0300

    ESPNParser spliited into Parser, ESPNHandler and ESPNGamecast. Logger trait added

---
+++ inc/ESPN/ESPNHandler.php
@@ -0,0 +1,80 @@
+<?php
+    
+namespace Robin\ESPN;
+
+use \Exception;
+use \Robin\Logger;
+
+class ESPNHandler
+{
+    use Logger;
+    
+    private $html;
+    
+    public $page_types = [ "Gamecast" => [ "engine" => "ESPNGamecast", "title" => "Game Summary"],
+                           "Rankings" => [ "engine" => "ESPNRankings", "title" => "Polls and Rankings"],
+                           "Standings" => [ "engine" => "ESPNStandings", "title" => "Standings"]];
+                           
+                               
+    public function __construct($html)
+    {
+        if (!$html || !in_array(get_class($html), [ "simple_html_dom", "simple_html_dom_node"])) {
+            throw new Exception("HTML DOM not received");
+        }
+        
+        $this->html = $html;
+        
+        $engine = $this->getPageEngine($this->getPageType());
+        
+        if (!$engine || !class_exists($engine)) {
+            throw new Exception('No engine found for the page');
+        }
+        
+        $this->engine = new $engine($html);
+    }
+    
+    private function getPageType()
+    {
+        $title = $this->html->find("head title", 0);
+        
+        if ($title) {
+            foreach ($this->page_types as $type => $values) {
+                if (mb_strpos($title->plaintext, $values["title"]) !== false) {
+                    return $type;
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    private function getPageEngine($type)
+    {
+        if (isset($this->page_types[$type]["engine"])) {
+            return "\\Robin\\ESPN\\".$this->page_types[$type]["engine"];
+        }
+        
+        return null;
+    }
+    
+    public function __get($name)
+    {
+        if (!is_numeric(name)) {
+            $capitalized_string = ucwords(str_replace("_", " ", $name));
+            $name = implode("", explode(" ", $capitalized_string));
+        }
+        
+        $method = "get" . $name;
+        
+        echo $method;
+        
+        /*
+        if (method_exists($this->engine, $method)) {
+            return $this->engine->$method;
+        }
+        */
+        
+        return null;
+    }
+    
+}
---
+++ inc/Parser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Robin;
+
+use \Exception;
+use \Robin\Logger;
+
+class Parser
+{
+    use Logger;
+    
+    public $engine_list = [ "\\ESPN\\ESPNHandler" => ["espn.com", "www.espn.com", "robin.local", "robin.firstandgoal.in"]];
+    public $engine;
+    
+    public function __construct($url)
+    {
+        require_once "simplehtmldom_1_9/simple_html_dom.php";
+        
+        // Checking if we have SimpleHTMLDOM loaded
+        if (!function_exists("file_get_html")) {
+            throw new Exception("Parsing function not defined");
+        }
+        
+        // Checking if we creating object with a valid URL
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new Exception('Invalid URL');
+        }
+        $domain = parse_url($url, PHP_URL_HOST);
+        
+        $engine = null;
+        
+        // Checking if URL belongs list of domains we can parse
+        foreach ($this->engine_list as $engine_key => $supported_domains) {
+            if (in_array($domain, $supported_domains)) {
+                $engine = '\Robin'.$engine_key;
+                break;
+            }
+        }
+
+        // Checking if engine was found and class exists
+        if (!$engine || !class_exists($engine)) {
+            throw new Exception('No engine found for the domain');
+        }
+        
+        $html = file_get_html($url);
+        
+        $this->engine = new $engine($html);
+    }
+
+}
