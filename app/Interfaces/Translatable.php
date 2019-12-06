03bbbcd71f80443029c161c02b387b742929cb10
Yuriy Marin (sprinthost) <yuriy.marin@gmail.com>
2019-12-06 18:38:07 +0300

    Fixed setLocale() description in Translatable interface

--- app/Interfaces/Translatable.php
+++ app/Interfaces/Translatable.php
@@ -4,7 +4,7 @@ namespace Robin\Interfaces;
 
 interface Translatable
 {
-    public function setLocale(string $locale): void;
+    public function setLocale(string $locale): bool;
     public function getLocale(): string;
     public function isTranslated($locale): bool;
     
