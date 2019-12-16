4973546313fe29cb590b703ca1488e04b2bbdaf8
Yuriy Marin (sprinthost) <yuriy.marin@gmail.com>
2019-12-16 18:27:47 +0300

    Fixed issue with commas in templates/leaders.php

--- templates/leaders.php
+++ templates/leaders.php
@@ -23,9 +23,9 @@
 </table>
 <textarea class="copy" id="leaders">[table caption=&quot;Лидеры статистики&quot;]
 Категория,<?=$home_team["name"]?>,<?=$away_team["name"]?><?=PHP_EOL?>
-Пас,<?=mb_substr($passing_leader["home_team"]["first_name"],0,1) . ". " . $passing_leader["home_team"]["last_name"]?> – <?=$passing_leader["home_team"]["stats"]?>,<?=mb_substr($passing_leader["away_team"]["first_name"],0,1) . ". " . $passing_leader["away_team"]["last_name"]?> – <?=$passing_leader["away_team"]["stats"]?><?=PHP_EOL?>
-Вынос,<?=mb_substr($rushing_leader["home_team"]["first_name"],0,1) . ". " . $rushing_leader["home_team"]["last_name"]?> – <?=$rushing_leader["home_team"]["stats"]?>,<?=mb_substr($rushing_leader["away_team"]["first_name"],0,1) . ". " . $rushing_leader["away_team"]["last_name"]?> – <?=$rushing_leader["away_team"]["stats"]?><?=PHP_EOL?>
-Прием,<?=mb_substr($receiving_leader["home_team"]["first_name"],0,1) . ". " . $receiving_leader["home_team"]["last_name"]?> – <?=$receiving_leader["home_team"]["stats"]?>,<?=mb_substr($receiving_leader["away_team"]["first_name"],0,1) . ". " . $receiving_leader["away_team"]["last_name"]?> – <?=$receiving_leader["away_team"]["stats"]?><?=PHP_EOL?>
+Пас,<?=mb_substr($passing_leader["home_team"]["first_name"],0,1) . ". " . $passing_leader["home_team"]["last_name"]?> – <?=str_replace(",", "\,", $passing_leader["home_team"]["stats"])?>,<?=mb_substr($passing_leader["away_team"]["first_name"],0,1) . ". " . $passing_leader["away_team"]["last_name"]?> – <?=str_replace(",", "\,", $passing_leader["away_team"]["stats"])?><?=PHP_EOL?>
+Вынос,<?=mb_substr($rushing_leader["home_team"]["first_name"],0,1) . ". " . $rushing_leader["home_team"]["last_name"]?> – <?=str_replace(",", "\,", $rushing_leader["home_team"]["stats"])?>,<?=mb_substr($rushing_leader["away_team"]["first_name"],0,1) . ". " . $rushing_leader["away_team"]["last_name"]?> – <?=str_replace(",", "\,", $rushing_leader["away_team"]["stats"])?><?=PHP_EOL?>
+Прием,<?=mb_substr($receiving_leader["home_team"]["first_name"],0,1) . ". " . $receiving_leader["home_team"]["last_name"]?> – <?=str_replace(",", "\,", $receiving_leader["home_team"]["stats"])?>,<?=mb_substr($receiving_leader["away_team"]["first_name"],0,1) . ". " . $receiving_leader["away_team"]["last_name"]?> – <?=str_replace(",", "\,", $receiving_leader["away_team"]["stats"])?><?=PHP_EOL?>
 [/table]
 </textarea>
 <button class="copy-text" data-target="leaders">Copy</button>
